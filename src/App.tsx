528a4ab7bfd0df4a35304b15aa23b387605d385b
Sara Montagud <43246362+Aforina@users.noreply.github.com>
2025-05-30 00:19:22 +0200

    feat: new chatbot design

--- src/App.tsx
+++ src/App.tsx
@@ -1,81 +1,13 @@
-import { ArrowRight, Loader } from "lucide-react";
-import { useState } from "react";
 import "./App.css";
 import ChatbotUI from "./components/chatbot";
+import { Preview } from "./components/Preview";
 
 function App() {
-  const [value, setValue] = useState("");
-  const [submitted, setSubmitted] = useState(false);
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    setSubmitted(true);
-  };
-
-  // if (response) {
-  //   return (
-  //     <section className="flex w-full gap-8 items-center justify-center h-screen">
-  //       {response?.map((item) => {
-  //         return (
-  //           <article>
-  //             <div
-  //               style={{ backgroundColor: item.color }}
-  //               className={`w-[150px] flex flex-col gap-2 h-[150px] ${
-  //                 colord(item.color).isLight() ? "text-black" : "text-white"
-  //               } rounded-md`}
-  //               key={item.value}
-  //             >
-  //               <span>{item.value}</span>
-  //               <span>{item.color}</span>
-  //             </div>
-  //             <p>{item.name}</p>
-  //           </article>
-  //         );
-  //       })}
-  //     </section>
-  //   );
-  // }
-
-  if (submitted) {
-    return <ChatbotUI initMessage={value} />;
-  }
-
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="flex flex-col w-full gap-8 items-center justify-center h-screen"
-    >
-      <label className="flex flex-col w-full gap-8">
-        <span className="text-5xl font-black text-center">
-          Let's start generating your branding
-        </span>
-
-        <textarea
-          value={value}
-          className="min-h-[160px] w-full max-h-[250px] p-4 border-2 border-slate-300 rounded-2xl"
-          placeholder="Tell me about your company and we'll start generate a branding for you..."
-          onChange={(event) => {
-            setValue(event.target.value);
-          }}
-        ></textarea>
-      </label>
-      <button
-        disabled={submitted}
-        className="w-full flex gap-2 items-center justify-center mx-4 cursor-pointer bg-slate-900 hover:bg-slate-800 text-white rounded-md py-4 px-4"
-      >
-        {submitted ? (
-          <>
-            Loading...
-            <Loader width={16} height={16} className="inline-block" />
-          </>
-        ) : (
-          <>
-            Start generate my branding
-            <ArrowRight width={16} height={16} className="inline-block" />
-          </>
-        )}
-      </button>
-    </form>
+    <main className="flex font-inter">
+      <ChatbotUI />;
+      <Preview />
+    </main>
   );
 }
 
--- src/components/chatbot.tsx
+++ src/components/chatbot.tsx
@@ -1,28 +1,16 @@
-import { useEffect, useRef, useState, type FormEvent } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useChatbot } from "../hooks/use-chatbot";
 import type { ChatMessage, FontItem, Palette } from "../types";
+import { PanelLeftClose, PanelLeftOpen, SendHorizonal } from "lucide-react";
 
-const SendIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="currentColor"
-    {...props}
-  >
-    <path d="M3.478 2.405a.75.75 0 00-.926.94l2.432 7.905H13.5a.75.75 0 010 1.5H4.984l-2.432 7.905a.75.75 0 00.926.94 60.519 60.519 0 0018.445-8.986.75.75 0 000-1.218A60.517 60.517 0 003.478 2.405z" />
-  </svg>
-);
-
-type Props = {
-  initMessage?: string;
-};
-
-export default function ChatbotUI({ initMessage }: Props) {
-  const { messages, isLoading, error, sendMessage, startNewChat, chatId } =
+export default function ChatbotUI() {
+  const { messages, isLoading, error, sendMessage, startNewChat } =
     useChatbot();
-  const [initMessageSent, setInitMessageSent] = useState(false);
   const [inputText, setInputText] = useState<string>("");
   const messagesEndRef = useRef<HTMLDivElement | null>(null);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  const hasMessages = messages.length > 0;
 
   const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
@@ -30,20 +18,8 @@ export default function ChatbotUI({ initMessage }: Props) {
 
   useEffect(scrollToBottom, [messages]);
 
-  useEffect(() => {
-    console.log("initMessageSent", initMessageSent);
-    if (initMessageSent) return;
-
-    if (initMessage) {
-      setInitMessageSent(true);
-      sendMessage(initMessage);
-    }
-  }, [initMessage]);
-
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (!inputText.trim()) return;
-    sendMessage(inputText);
+  const handleMessage = (message: string) => {
+    sendMessage(message);
     setInputText("");
   };
 
@@ -102,96 +78,146 @@ export default function ChatbotUI({ initMessage }: Props) {
   };
 
   return (
-    <div className="w-full max-w-2xl mx-auto h-[700px] flex flex-col bg-white dark:bg-gray-800 shadow-xl rounded-lg border border-gray-200 dark:border-gray-700">
+    <div
+      className={`${
+        isCollapsed ? "w-[100px]" : "w-[500px]"
+      } min-h-screen flex flex-col bg-background p-5 gap-8 duration-200 ease-in-out`}
+    >
       {/* Header */}
-      <div className="bg-blue-600 dark:bg-blue-700 text-white p-4 rounded-t-lg flex justify-between items-center">
-        <div>
-          <h2 className="text-xl font-semibold">Chatbot</h2>
-          {chatId && (
-            <p className="text-xs text-blue-200 dark:text-blue-300">
-              ID: {chatId}
-            </p>
-          )}
-        </div>
-        <button
-          onClick={startNewChat}
-          className="bg-blue-700 hover:bg-blue-800 dark:bg-blue-800 dark:hover:bg-blue-900 text-white text-sm py-1.5 px-3 rounded-md transition-colors"
-        >
-          Nuevo Chat
-        </button>
-      </div>
-
-      {/* Messages Area */}
-      <div className="flex-grow p-4 space-y-4 overflow-y-auto bg-gray-50 dark:bg-gray-900">
-        {messages.map((msg) => (
-          <div
-            key={msg.id}
-            className={`flex ${
-              msg.sender === "user" ? "justify-end" : "justify-start"
-            }`}
+      <div className="flex shadow-sm bg-white px-[10px] py-[5px] rounded-md justify-between items-center gap-4">
+        <hgroup className="flex gap-2 items-center">
+          <img
+            src={"/imgs/Hambot.webp"}
+            alt="Hambot Logo"
+            width={36}
+            height={36}
+          />
+          <h1 className="font-bold">Hambot</h1>
+        </hgroup>
+        <div className="flex gap-2">
+          <button
+            onClick={startNewChat}
+            className="bg-slate-200 w-fit hover:bg-slate-300 text-cs-black text-sm py-1.5 px-3 rounded-md transition-colors"
           >
-            <div
-              className={`max-w-[75%] p-3 rounded-xl shadow ${
-                msg.sender === "user"
-                  ? "bg-blue-500 text-white rounded-br-none"
-                  : msg.type === "error"
-                  ? "bg-red-100 dark:bg-red-700 text-red-700 dark:text-red-100 border border-red-300 dark:border-red-600 rounded-bl-none"
-                  : "bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded-bl-none"
-              }`}
-            >
-              {renderMessageContent(msg)}
-              <p
-                className={`text-xs mt-1.5 ${
-                  msg.sender === "user"
-                    ? "text-blue-200 text-right"
-                    : "text-gray-500 dark:text-gray-400 text-right"
+            Nuevo Chat
+          </button>
+          <button
+            onClick={() => {
+              setIsCollapsed(!isCollapsed);
+            }}
+            className="text-primary cursor-pointer hover:bg-slate-100 rounded-md p-2"
+          >
+            {isCollapsed ? <PanelLeftOpen /> : <PanelLeftClose />}
+          </button>
+        </div>
+      </div>
+      {/* Messages Area */}
+      <div className="flex-grow p-4 overflow-y-auto overflow-x-hidden">
+        {!hasMessages ? (
+          <div className="flex flex-col h-full justify-center relative">
+            {/* Empty State */}
+            <div className="absolute z-10 flex items-center justify-center inset-0">
+              <div className="h-fit max-w-xs flex flex-col gap-4 items-center p-8 rounded-lg drop-shadow-md bg-white ">
+                <img
+                  src="/imgs/Palettes-Chat.webp"
+                  alt="Decoration Palettes"
+                  width={180}
+                  className="drop-shadow-md"
+                />
+                <p className="font-bold leading-8 max-w-[200px] text-[28px] text-center text-cs-black">
+                  Let's start to see your palettes here
+                </p>
+                <p className="text-sm  leading-4 max-w-[200px] text-center text-description">
+                  Explore the canvas to see what you could create!
+                </p>
+              </div>
+            </div>
+            <img
+              src="/imgs/Cloud.webp"
+              alt="Decoration Cloud"
+              width={542}
+              height={353}
+              className="scale-[1.1]"
+            />
+          </div>
+        ) : (
+          <>
+            {messages.map((msg) => (
+              <div
+                key={msg.id}
+                className={`flex ${
+                  msg.sender === "user" ? "justify-end" : "justify-start"
                 }`}
               >
-                {new Date(msg.timestamp).toLocaleTimeString([], {
-                  hour: "2-digit",
-                  minute: "2-digit",
-                })}
-              </p>
-            </div>
-          </div>
-        ))}
-        {isLoading && (
-          <div className="text-center text-sm text-gray-500 dark:text-gray-400 italic py-2">
-            Escribiendo...
-          </div>
+                <div
+                  className={`p-3 rounded-xl shadow ${
+                    msg.sender === "user"
+                      ? "bg-blue-500 text-white rounded-br-none"
+                      : msg.type === "error"
+                      ? "bg-red-100 dark:bg-red-700 text-red-700 dark:text-red-100 border border-red-300 dark:border-red-600 rounded-bl-none"
+                      : "bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded-bl-none"
+                  }`}
+                >
+                  {renderMessageContent(msg)}
+                  <p
+                    className={`text-xs mt-1.5 ${
+                      msg.sender === "user"
+                        ? "text-blue-200 text-right"
+                        : "text-gray-500 dark:text-gray-400 text-right"
+                    }`}
+                  >
+                    {new Date(msg.timestamp).toLocaleTimeString([], {
+                      hour: "2-digit",
+                      minute: "2-digit",
+                    })}
+                  </p>
+                </div>
+              </div>
+            ))}
+            {isLoading && (
+              <div className="text-center text-sm text-gray-500 dark:text-gray-400 italic py-2">
+                Escribiendo...
+              </div>
+            )}
+            <div ref={messagesEndRef} />
+          </>
         )}
-        <div ref={messagesEndRef} />
       </div>
-
       {/* Error Display */}
       {error && (
         <div className="p-3 bg-red-100 dark:bg-red-800 text-red-700 dark:text-red-200 text-sm text-center border-t border-red-200 dark:border-red-700">
           Error: {error}
         </div>
       )}
-
       {/* Input Area */}
       <form
-        onSubmit={handleSubmit}
-        className="p-4 border-t border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 rounded-b-lg"
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (!inputText.trim()) return;
+          handleMessage(inputText);
+        }}
+        className="relative"
       >
-        <div className="flex items-center space-x-2">
-          <input
-            type="text"
-            value={inputText}
-            onChange={(e) => setInputText(e.target.value)}
-            placeholder="Escribe tu mensaje..."
-            className="flex-grow p-3 border border-gray-300 dark:border-gray-600 rounded-full focus:ring-2 focus:ring-blue-500 focus:border-transparent outline-none bg-gray-50 dark:bg-gray-700 text-gray-900 dark:text-gray-100 placeholder-gray-400 dark:placeholder-gray-500"
-            disabled={isLoading}
-          />
-          <button
-            type="submit"
-            className="bg-blue-600 hover:bg-blue-700 dark:bg-blue-700 dark:hover:bg-blue-800 text-white p-3 rounded-full disabled:opacity-50 disabled:cursor-not-allowed transition-colors"
-            disabled={isLoading}
-          >
-            <SendIcon className="w-6 h-6" />
-          </button>
-        </div>
+        <textarea
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+          onKeyDown={(event) => {
+            if (event.key === "Enter") {
+              if (!inputText.trim()) return;
+              handleMessage(inputText);
+            }
+          }}
+          placeholder="Let's build your corporate brand"
+          className="flex-grow  max-h-[175px] field-sizing-content border border-border flex items-start bg-white outline-none placeholder:color-placeholder min-h-20 p-3 rounded-md w-full "
+          disabled={isLoading}
+        />
+        <button
+          type="submit"
+          className="absolute w-8 h-8 flex items-center justify-center bottom-2 right-2 cursor-pointer bg-dark hover:bg-[#104C6B] p-2 rounded-full transition-colors"
+          disabled={isLoading}
+        >
+          <SendHorizonal color="#ffffff" width={12} height={14} />
+        </button>
       </form>
     </div>
   );
