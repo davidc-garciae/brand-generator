545e582b01426c6a417053223195121b3377e960
Freilyn Bernabe <freilyn@bernabe.dev>
2025-05-30 17:34:17 -0400

    feat: Implement authentication (login, register, logout, me)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import "./App.css";
+import Login from "./components/auth/login";
+import Register from "./components/auth/register";
 import ChatbotUI from "./components/chatbot";
 import { Preview } from "./components/Preview";
+import { useAuth } from "./hooks/use-auth";
 
 function App() {
+  const pathname = window.location.pathname;
+  const { user } = useAuth();
+  console.log({ user }); //Testing current user
+
+  if (pathname === "/register") {
+    return <Register />;
+  }
+
+  if (pathname === "/login") {
+    return <Login />;
+  }
+
   return (
     <main className="flex font-inter">
       <ChatbotUI />;
