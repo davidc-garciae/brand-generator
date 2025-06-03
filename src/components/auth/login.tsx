1a7722124eb1dab4ca1bde9cea18899ca3c91ed4
developerleonardo <leo.11hnz@gmail.com>
2025-06-02 19:37:07 -0500

    feat: Enhance Login component layout and add illustrative images

--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -30,26 +30,52 @@ export default function Login() {
   }
 
   return (
-    <div>
-      <form onSubmit={onSubmit} className="flex w-96 flex-col gap-9 rounded-lg bg-white p-6 shadow-md">
-        <h2 className="text-center font-bold">Login</h2>
-        <div className="flex w-full flex-col gap-4">
-          <Input type="email" name="email" placeholder="user@example.com" label="Email" />
-          <Input type="password" name="password" placeholder="Your password" label="Password" resend={true} />
-          <Button type="submit" variant="primary" label="Login" />
+    <>
+      <div className="absolute -z-10 h-1/2 w-full bg-purple-100"></div>
+      <div className="col-span-2 m-auto grid min-h-screen w-full max-w-[1440px] grid-cols-[1fr_400px] px-32 py-48">
+        <div className="col-start-1 flex h-full w-full">
+          <div className="h-1/3 w-full">
+            <div className="flex h-full w-full items-start gap-14">
+              <div className="text-cs-black flex w-[424px] flex-col justify-center gap-3">
+                <h1 className="text-2xl font-bold">From prompt to complete corporate branding</h1>
+                <p>
+                  Colors, typography, and brand personality made simple. Perfect for entrepreneurs, freelancers, and
+                  creatives. Just answer a few questions, and we’ll make magic!
+                </p>
+              </div>
+              <div className="relative">
+                <img src="/imgs/Hambot.webp" alt="hambot image" className="h-[127px] w-[127px]" />
+                <img src="/imgs/changeme-1.webp" alt="color illustration" className="absolute top-[-86px] -left-12" />
+                <img src="/imgs/changeme-2.webp" alt="color illustration" className="absolute -bottom-26 left-6" />
+                <img src="/imgs/changeme-3.webp" alt="color illustration" className="absolute top-[-86px] left-32" />
+              </div>
+            </div>
+          </div>
+          <div></div>
         </div>
-        <p className="text-center">Or</p>
-        <div className="flex w-full flex-col gap-3">
-          <Button icon={<GoogleIcon />} variant="secondary" label="Continue with Google" />
-          <Button icon={<GitHubIcon />} variant="secondary" label="Continue with GitHub" />
-        </div>
-        <p className="text-cs-black text-center text-sm">
-          Don't have an account?{' '}
-          <a href="/register" className="text-cs-black font-bold hover:underline">
-            Sign Up
-          </a>
-        </p>
-      </form>
-    </div>
+        <form
+          onSubmit={onSubmit}
+          className="col-start-2 flex h-fit w-96 flex-col gap-9 place-self-center rounded-3xl bg-white p-6 shadow-md"
+        >
+          <h2 className="text-center font-bold">Login</h2>
+          <div className="flex w-full flex-col gap-4">
+            <Input type="email" name="email" placeholder="user@example.com" label="Email" />
+            <Input type="password" name="password" placeholder="Your password" label="Password" resend={true} />
+            <Button type="submit" variant="primary" label="Login" />
+          </div>
+          <p className="text-center">Or</p>
+          <div className="flex w-full flex-col gap-3">
+            <Button icon={<GoogleIcon />} variant="secondary" label="Continue with Google" />
+            <Button icon={<GitHubIcon />} variant="secondary" label="Continue with GitHub" />
+          </div>
+          <p className="text-cs-black text-center text-sm">
+            Don't have an account?{' '}
+            <a href="/register" className="text-cs-black font-bold hover:underline">
+              Sign Up
+            </a>
+          </p>
+        </form>
+      </div>
+    </>
   )
 }
